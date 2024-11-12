ae5411f06433c8cbaece0bd087242358db8f8e1e
Rupa <rupa@Luthfuns-MacBook-Pro.local>
2024-11-12 13:07:36 +0600

    Initial commit for AgencyDataApi project

---
+++ src/main/java/com/example/agencydataapi/model/AgencyRefData.java
@@ -0,0 +1,17 @@
+package com.example.agencydataapi.model;
+public class AgencyRefData {
+    private String name;
+    private String code;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
